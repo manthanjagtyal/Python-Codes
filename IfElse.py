7eb50c17070ba11ff27236cfff73dab5b03cf493
Manthan Sharma <msjagtyal@gmail.com>
2025-11-05 19:38:25 +0530

    Create IfElse.py
    Most Basic If Else Statement Codes

---
+++ IfElse.py
@@ -0,0 +1,13 @@
+a=int(input("Enter age: "))
+print("Your age is ",a)
+if a>=18:
+    print("You can Drive!")
+else:
+    print("You can't Drive!")
+b=int(input("Enter number: "))
+if b>0:
+    print("Number is Positive.")
+elif b==0:
+    print("Number is Zero.")
+else:
+    print("Number is Negative.")
