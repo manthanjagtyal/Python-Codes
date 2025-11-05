f2e008a606ea5e7205276dbcc1384eea4c06e969
Manthan Sharma <msjagtyal@gmail.com>
2025-11-05 19:25:58 +0530

    Add f-strings examples for formatted output
    Basic idea about f-strings

---
+++ f-strings.py
@@ -0,0 +1,8 @@
+x="Hi my name is {} and my country is {}"
+name="Manthan"
+country="India"
+print(x.format(name,country))
+#Alternate way
+print(f"Hi my name is {name} and my country is {country}.")
+x=float(input("Enter any decimal: "))
+print(f"2 digits decimals of {x} is {x:.2f}")
